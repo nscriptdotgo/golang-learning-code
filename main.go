603dab5b1b912254abf0418a76434c03b6d841a1
Noah Dlugoszewski <noahjdlugoszewski@gmail.com>
2021-01-17 15:50:31 -0600

    Initial commit.

---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+const (
+	BaseURL = "https://api.openweathermap.org/data/2.5"
+	Unit    = "imperial"
+)
+
+type Coord struct {
+	Lon float64 `json:"lon"`
+	Lat float64 `json:"lat"`
+}
+
+type Weather struct {
+	ID          int    `json:"id"`
+	Main        string `json:"main"`
+	Description string `json:"description"`
+	Icon        string `json:"icon"`
+}
+type Main struct {
+	Temp      float64 `json:"temp"`
+	FeelsLike float64 `json:"feels_like"`
+	TempMin   float64 `json:"temp_min"`
+	TempMax   float64 `json:"temp_max"`
+	Pressure  int     `json:"pressure"`
+	Humidity  int     `json:"humidity"`
+}
+type Wind struct {
+	Speed float64 `json:"speed"`
+	Deg   int     `json:"deg"`
+}
+
+type Clouds struct {
+	All int `json:"all"`
+}
+
+type Sys struct {
+	Type    int     `json:"type"`
+	ID      int     `json:"id"`
+	Message float64 `json:"message"`
+	Country string  `json:"country"`
+	Sunrise int     `json:"sunrise"`
+	Sunset  int     `json:"sunset"`
+}
+
+type SimpleCurrentWeatherData struct {
+	Name      string `json:"name"`
+	Base      string `json:"base"`
+	FeelsLike string `json:"feels_like"`
+}
+
+type CurrentWeatherData struct {
+	Coord      Coord     `json:"coords"`
+	Weather    []Weather `json:"weather"`
+	Base       string    `json:"base"`
+	Main       Main      `json:"main"`
+	Visibility int       `json:"visibility"`
+	Wind       Wind      `json:"wind"`
+	Clouds     Clouds    `json:"clouds"`
+	Dt         int       `json:"dt"`
+	Sys        Sys       `json:"sys"`
+	Timezone   int       `json:"timezone"`
+	ID         int       `json:"id"`
+	Name       string    `json:"name"`
+	Cod        int       `json:"cod"`
+}
+
+func getCurrentWeatherByZipCode(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// Declare and intialize vars needed for api call, map mux route parameter
+	token := os.Getenv("TOKEN")
+	vars := mux.Vars(r)
+	zipCode := vars["zipCode"]
+	// Make API call with above vars, returns Response object
+	resp, err := http.Get(fmt.Sprintf("%s/weather?zip=%s,us&units=%s&appid=%s", BaseURL, zipCode, Unit, token))
+	log.Print(resp.Request)
+	// catch error if error
+	if err != nil {
+		log.Fatal(err.Error())
+		os.Exit(1)
+	}
+	// Take response object and covert that into a byte slice - we need this to store the values into custom struct for currentweatherdata
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println(string(responseData))
+	// declare weatherDataObject var as type CurrentWeatherData
+	var weatherDataObject CurrentWeatherData
+	// Unmarshal the byte slice and store into new weatherDataObject var
+	json.Unmarshal(responseData, &weatherDataObject)
+
+	json.NewEncoder(w).Encode(weatherDataObject)
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/currentweather/{zipCode}", getCurrentWeatherByZipCode)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func main() {
+	log.Print("==== Starting OpenWeather Golang Custom API ====")
+	log.Print("Loading dotenv variables from file...")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	log.Print("Environment variables loaded!")
+	log.Print("==== Started OpenWeather Golang Custom API ====")
+	handleRequests()
+}
