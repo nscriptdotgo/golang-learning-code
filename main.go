df8796834ad5c946f3d51721ada78c7fd84dc00b
Noah Dlugoszewski <noahjdlugoszewski@gmail.com>
2021-01-18 17:04:51 -0600

    testing change.

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ type Weather struct {
 // Main struct, contains the actual weather data
 type Main struct {
 	Temp      float64 `json:"temp"`
-	FeelsLike float64 `json:"feels_like"`
+	FeelsLike float64 `json:"feels_like_a_burrito"`
 	TempMin   float64 `json:"temp_min"`
 	TempMax   float64 `json:"temp_max"`
 	Pressure  int     `json:"pressure"`
